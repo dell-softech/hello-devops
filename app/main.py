650daf4d131ebeb0ad9fa9480362aa3309793e5f
Dell <dellsoftech@gmail.com>
2026-07-11 08:05:10 +0500

    Create first Flask application

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-print("Feature branch")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello DevOps!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
